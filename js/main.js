dcba43ff11e1f0cb648c827d41f19392020091c8
SHIVAM ASATI <49972593+shivam019@users.noreply.github.com>
2022-12-30 19:41:15 -0800

    changes to src winner page

--- js/main.js
+++ js/main.js
@@ -142,6 +142,25 @@ jQuery(document).ready(function( $ ) {
     modal.find('#ticket-type').val(ticketType);
   })
 
-// custom code
+// SRC Cards
+var scrollTop = 0;
 
+$(".content-wrapper").scroll(function () {
+	if ($(this).scrollTop() > 300) {
+		$(".menu-bar-wrapper").css('bottom', -$(this).scrollTop());
+		return false;
+	}
+	$(".menu-bar-wrapper").css('bottom', -$(this).scrollTop());
+	var st = jQuery(this).scrollTop();
+
+	if (st > 20) {
+		if (st < scrollTop) {
+			jQuery(".menu-bar").addClass("active");
+		} else {
+			jQuery(".menu-bar").removeClass("active");
+		}
+		scrollTop = st;
+	}
+
+});
 });
